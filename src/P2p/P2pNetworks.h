7bb05abd4101c17db6bfccc3fdea8d0dfff78e24
mahditavakoly <mahditavakoly@gmail.com>
2023-04-16 23:51:22 +0430

    Upload project Files

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x03, 0xc3, 0xb3, 0x70, 0x1a, 0xa0, 0xad, 0x78, 0xdf, 0x9f, 0x3f, 0x83, 0x77, 0x16, 0x7a, 0x02 } };
+}
