3aa0313384d762801cba8d3c4e592d5cb730b668
mihancoin <130921776+mihancoin@users.noreply.github.com>
2023-04-18 17:52:53 +0330

    Genesis tx updated

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "mihancoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210146bbc82ff9fb4f9db5cba79d160e11a5c83f1975177e24bd72c5332554e5e323";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101537b079b41c7bca851d2cdfe8ab4d9e7da3c6c4eff9286d0a2f52ca01358baf2";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
